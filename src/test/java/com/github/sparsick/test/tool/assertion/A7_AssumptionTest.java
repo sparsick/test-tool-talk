9de027549c3ba7573ce379a74d49b07e147a8793
sparsick <sparsick@web.de>
2019-10-16 10:00:19 +0200

    add more sample for assumptions

--- src/test/java/com/github/sparsick/test/tool/assertion/A7_AssumptionTest.java
+++ src/test/java/com/github/sparsick/test/tool/assertion/A7_AssumptionTest.java
@@ -2,6 +2,9 @@ package com.github.sparsick.test.tool.assertion;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.util.List;
+
 import static org.assertj.core.api.Assumptions.assumeThat;
 
 class A7_AssumptionTest {
@@ -9,5 +12,15 @@ class A7_AssumptionTest {
     @Test
     void assume(){
         assumeThat("Bonn").isEqualTo("Bonn");
+    }
+
+    @Test
+    void assumeMoreComplex(){
+        assumeThat(new File("/starwars-testdata/star-wars-logo.png"))
+                .isFile()
+                .exists();
+        assumeThat(List.of("Hello", "World"))
+                .hasSize(2)
+                .contains("Hello");
     }
 }
