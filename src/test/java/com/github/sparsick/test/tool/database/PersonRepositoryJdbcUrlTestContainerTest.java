8874b9e2fb449929ea59c069e37aedadf165ddef
sparsick <sparsick@web.de>
2020-07-01 06:51:17 +0200

    add jdbcurl sample for testcontainers

---
+++ src/test/java/com/github/sparsick/test/tool/database/PersonRepositoryJdbcUrlTestContainerTest.java
@@ -0,0 +1,55 @@
+package com.github.sparsick.test.tool.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PersonRepositoryJdbcUrlTestContainerTest {
+    private PersonRepository repositoryUnderTest;
+    private Flyway flyway;
+
+    @BeforeEach
+    void setup(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:tc:postgresql:9.6.8:///persondb");
+        hikariConfig.setUsername("postgres");
+        hikariConfig.setPassword("");
+
+        DataSource ds = new HikariDataSource(hikariConfig);
+        flyway = Flyway.configure().dataSource(ds).load();
+        flyway.migrate();
+
+        repositoryUnderTest = new PersonRepository(ds);
+
+    }
+
+    @AfterEach
+    void cleanUp(){
+        flyway.clean();
+    }
+
+
+    @Test
+    void saveAndFindAllPerson() {
+        Person person = new Person();
+        person.setFirstName("firstName");
+        person.setLastName("lastName");
+        person.setJobTitle("jobTitle");
+
+        repositoryUnderTest.save(person);
+
+        List<Person> persons = repositoryUnderTest.findAllPersons();
+        assertThat(persons).hasSize(1).contains(person);
+    }
+
+}
+
+
