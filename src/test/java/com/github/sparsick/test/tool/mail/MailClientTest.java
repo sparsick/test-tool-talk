df5737ab3caed9ef35361a1f626d56ac30588953
sparsick <sparsick@web.de>
2020-11-05 07:41:23 +0100

    email - migrate to junit 5 extension

--- src/test/java/com/github/sparsick/test/tool/mail/MailClientTest.java
+++ src/test/java/com/github/sparsick/test/tool/mail/MailClientTest.java
@@ -1,50 +1,64 @@
 package com.github.sparsick.test.tool.mail;
 
 import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.user.GreenMailUser;
 import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetup;
-import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.util.SocketUtils;
 
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class MailClientTest {
 
-    private static ServerSetup smtp;
-    private GreenMail greenMail;
+    private static int smtpPort = SocketUtils.findAvailableTcpPort();
+    private static final ServerSetup SERVER_SETUP_SMTP = new ServerSetup(smtpPort, null, ServerSetup.PROTOCOL_SMTP);
+    private static int pop3Port = SocketUtils.findAvailableTcpPort();
+    private static final ServerSetup SERVER_SETUP_POP3 = new ServerSetup(pop3Port, null, ServerSetup.PROTOCOL_POP3);
+
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(new ServerSetup[]{
+            SERVER_SETUP_SMTP,
+            SERVER_SETUP_POP3});
+
+
     private MailClient clientUnderTest;
 
     @BeforeAll
     static void setUpAll() {
-        int portOffset = ThreadLocalRandom.current().nextInt(1000, 2000);
-        smtp = new ServerSetup(25 + portOffset, null, ServerSetup.PROTOCOL_SMTP);
+        greenMail.withConfiguration(GreenMailConfiguration.aConfig().withUser("user@example.com","test", "xxx"));
+    }
+
+    @AfterEach
+    void cleanUp(){
+        greenMail.reset();
     }
 
     @BeforeEach
     void setUp() {
-        greenMail = new GreenMail(smtp);
-        JavaMailSender javaMailSender = createJavaMailSender();
-        clientUnderTest = new MailClient(javaMailSender);
-        greenMail.withConfiguration(GreenMailConfiguration.aConfig().withUser("test", "xxx"));
-        greenMail.start();
+        Properties properties = createProperties();
+        clientUnderTest = new MailClient(properties, "test", "xxx");
     }
 
     @Test
     void sendMessage() throws MessagingException, IOException {
         String message = "text";
-        String from = "from@example.com";
+        String from = "user@example.com";
         String to = "to@example.com";
         clientUnderTest.sendMessage(from, to, message);
 
@@ -52,27 +66,33 @@ class MailClientTest {
         assertThat(receivedMessages).hasSize(1);
         String content = (String) receivedMessages[0].getContent();
         assertThat(content.trim()).isEqualTo(message);
-
     }
 
-    @AfterEach
-    void cleanUp() {
-        greenMail.stop();
+    @Test
+    void receiveMessages(){
+        deliverMessage();
+
+        List<Message> messages = clientUnderTest.receiveMessages();
+
+        assertThat(messages).hasSize(1);
     }
 
-    @NotNull
-    private JavaMailSenderImpl createJavaMailSender() {
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+    private void deliverMessage() {
+        GreenMailUser user = greenMail.setUser("user@example.com","test", "xxx");
+        MimeMessage message = GreenMailUtil.createTextEmail("user@example.com", "from@localhost.com", "A subject", "some text", SERVER_SETUP_POP3);
+        user.deliver(message);
+    }
+
+    private Properties createProperties() {
         Properties props = new Properties();
         props.setProperty("mail.transport.protocol", "smtp");
-        props.setProperty("mail.smtp.port", String.valueOf(smtp.getPort()));
+        props.setProperty("mail.smtp.port", String.valueOf(smtpPort));
         props.setProperty("mail.smtp.auth", "true");
-        props.setProperty("mail.smtp.user", "from@example.com");
+        props.setProperty("mail.smtp.user", "user@example.com");
         props.setProperty("mail.smtp.host", "localhost");
-        props.setProperty("mail.smtp.from", "from@example.com");
-        javaMailSender.setJavaMailProperties(props);
-        javaMailSender.setUsername("test");
-        javaMailSender.setPassword("xxx");
-        return javaMailSender;
+        props.setProperty("mail.smtp.from", "user@example.com");
+        props.setProperty("mail.pop3.host", "localhost");
+        props.setProperty("mail.pop3.port", String.valueOf(pop3Port));
+        return props;
     }
 }
